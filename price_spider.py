4467322a1bbe7f073cce367c0249984ca2c06ae0
WZJ <2449172727@qq.com>
2020-08-31 21:13:04 +0800

    更新价格获取代码

--- price_spider.py
+++ price_spider.py
@@ -1,13 +1,10 @@
 import requests
-from urllib.request import urlretrieve
 import re
-import pytesseract
-from PIL import Image
 from lxml import etree
 import image_ocr
 
 # 请求的url
-url = "http://sz.ziroom.com/x/794177397.html"
+url = "http://cd.ziroom.com/x/807596134.html"
 
 headers = {
     'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
@@ -17,19 +14,20 @@ headers = {
 response = requests.get(url, headers=headers)
 
 html = etree.HTML(response.text)
-bg_img_info = html.xpath('//div[@class="Z_price"]/i[@class="num"]/@style')[0]
-if len(re.findall(r'background-image: url\((.*?)\)',bg_img_info)) > 0:
-    bg_img_url = "http:" + re.findall(r'background-image: url\((.*?)\)',bg_img_info)[0]
-    info = html.xpath('//div[@class="Z_list-box"]/div[@class="item"]')
-    for i in info:
-        titles = i.xpath('.//h5/a/text()')
-        if titles:
-            title = titles[0]
-        position_list = [re.findall(r'background-position: -(.*?)px',j)[0] for j in i.xpath('.//div[@class="price "]/span[@class="num"]/@style')]
-        price_list = [int(float(i)/21.4 + 1) for i in position_list]
-        text = image_ocr.image_ocr(bg_img_url)
-        num = [i for i in text]
-        price = "￥" + "".join([num[i-1] for i in price_list]) + "/月起"
-        print(title, price)
+bg_img_info = html.xpath('//div[@class="Z_price"]/i[@class="num"]/@style')
+if len(re.findall(r'background-image: url\((.*?)\)',bg_img_info[0])) > 0:
+    bg_img_url = "http:" + re.findall(r'background-image: url\((.*?)\)',bg_img_info[0])[0]
+    position_list = []
+    text = image_ocr.image_ocr(bg_img_url)
+    price_list = []
+    for i in bg_img_info:
+        position = re.findall(r'background-position:-(.*?)px',i)[0]
+        position_list.append(position)
+    for i in position_list:
+        price_list.append(int(float(i)/30 + 1))
+
+    num = [i for i in text]
+    price = "￥" + "".join([num[i-1] for i in price_list]) + "/月起"
+    print(price)
 else:
       print("提取背景图片链接出错！")
